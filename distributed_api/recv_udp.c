f374d165a98ba19a8e61b0eed8bb426dd97022f0
kartiknesari <kartiknesari@gmail.com>
2024-04-17 23:53:52 -0400

    Added readme file

--- distributed_api/recv_udp.c
+++ distributed_api/recv_udp.c
@@ -337,7 +337,7 @@ int main()
         }
         for (int i = 0; i < 20; i++)
         {
-            sleep(1);
+            // sleep(1);
             if (!distributed_mutex_lock())
             {
                 switch (host_id)
